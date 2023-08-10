e0642da41e1a25fae11320ddc5e7d9642945e75a
pierreviences <imaniyazid@gmail.com>
2023-08-10 15:47:32 +0700

    feat: declaration variabel

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.calculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.Expression
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var lastNumeric = false
+    var stateError = false
+    var lastDot = false
+
+    private lateinit var expression: Expression
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 
     fun onAllClearClick(view: View) {}
@@ -16,4 +26,8 @@ class MainActivity : AppCompatActivity() {
     fun onOperatorClick(view: View) {}
     fun onBackClick(view: View) {}
     fun onClearClick(view: View) {}
+
+    fun OnEqual(){
+
+    }
 }
